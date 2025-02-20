c4d190ba264f4717c9079babcc2ee2b50df5b1b2
hieuvolaptrinh <vndhieuak@gmail.com>
2025-02-20 21:35:15 +0700

    first commit

---
+++ Fashion-Store/src/test/java/Fashion_Store/Fashion_Store/FashionStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package Fashion_Store.Fashion_Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
